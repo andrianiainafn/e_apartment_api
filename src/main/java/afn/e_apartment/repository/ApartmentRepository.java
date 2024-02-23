ad41ca23b5f62f83e5d2d353afdc4a9f3f1b4d29
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-23 23:44:02 +0300

    feature : make the api accessible for the frontend

--- src/main/java/afn/e_apartment/repository/ApartmentRepository.java
+++ src/main/java/afn/e_apartment/repository/ApartmentRepository.java
@@ -3,7 +3,12 @@ package afn.e_apartment.repository;
 import afn.e_apartment.model.Apartment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RepositoryRestResource
+@CrossOrigin (origins = "http://localhost:3000",
+        methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE },
+        maxAge = 3600)
 public interface ApartmentRepository extends JpaRepository<Apartment,String> {
 }
