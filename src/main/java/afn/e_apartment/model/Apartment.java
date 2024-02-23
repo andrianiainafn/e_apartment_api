7bf8adda96d4f2393e0a68db44846d9ddff29df5
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-23 23:42:21 +0300

    fix: id error

--- src/main/java/afn/e_apartment/model/Apartment.java
+++ src/main/java/afn/e_apartment/model/Apartment.java
@@ -2,6 +2,7 @@ package afn.e_apartment.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 public class Apartment {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
     private String numApp;
     private String design;
     private int rent;
