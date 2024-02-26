1e7760327826fd29457921e2485f8132d1436583
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-26 19:36:15 +0300

    feature : add controller for stat data

--- src/main/java/afn/e_apartment/controller/ApartmentController.java
+++ src/main/java/afn/e_apartment/controller/ApartmentController.java
@@ -1,7 +1,24 @@
 package afn.e_apartment.controller;
 
+import afn.e_apartment.dto.ApartmentStatDto;
+import afn.e_apartment.service.ApartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApartmentController {
+    private  final ApartmentService apartmentService;
+
+    public ApartmentController(ApartmentService apartmentService) {
+        this.apartmentService = apartmentService;
+    }
+
+    @CrossOrigin
+    @GetMapping("/stat/apartment")
+    public ResponseEntity<ApartmentStatDto> getStatForApartment(){
+        return new ResponseEntity<>(apartmentService.getApartmentStat( ), HttpStatus.OK);
+    }
 }
