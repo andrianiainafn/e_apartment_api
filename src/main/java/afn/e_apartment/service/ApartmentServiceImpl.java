fdbcd23fd3df881fbeb2cf60419c596b3fec58c5
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-26 19:36:57 +0300

    feature : implement service for stat data

--- src/main/java/afn/e_apartment/service/ApartmentServiceImpl.java
+++ src/main/java/afn/e_apartment/service/ApartmentServiceImpl.java
@@ -1,9 +1,21 @@
 package afn.e_apartment.service;
 
+import afn.e_apartment.dto.ApartmentStatDto;
+import afn.e_apartment.repository.ApartmentRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class ApartmentServiceImpl implements ApartmentService{
+    private final ApartmentRepository apartmentRepository;
+
+    public ApartmentServiceImpl(ApartmentRepository apartmentRepository) {
+        this.apartmentRepository = apartmentRepository;
+    }
+
+    @Override
+    public ApartmentStatDto getApartmentStat() {
+        return apartmentRepository.getStatForApartment();
+    }
 }
